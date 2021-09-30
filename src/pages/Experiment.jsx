cb6f3eb62465a79a74cf885f3795793fd23d6a8c
JorgBekelaar <j.w.r.bekelaar@student.tue.nl>
2021-09-30 15:51:45 +0200

    Walls & characterset Pausch room

--- src/pages/Experiment.jsx
+++ src/pages/Experiment.jsx
@@ -1,35 +1,28 @@
-import { Plane, Sky, Text } from '@react-three/drei'
+import { Plane, Text } from '@react-three/drei'
 import { DefaultXRControllers, VRCanvas } from '@react-three/xr'
 import ReactDOM from 'react-dom'
 import Head from '../components/Head'
 import { PositionLogger } from '../components/vr/PositionLogger'
 import '../index.css'
 
-function Floor(props) {
-	return (
-		<Plane rotation={[-Math.PI / 2, 0, 0]} scale={10} {...props}>
-			<meshPhongMaterial attach='material' color='#666' />
-		</Plane>
-	)
-}
+const characterSet1 = ['A', 'K', 'M', 'N', 'V', 'W', 'X', 'Y', 'Z'];
+const characterSet2 = ['E', 'F', 'H', 'I', 'L', 'T'];  
 
-function Wall({ color, numTargets = [10, 10], ...restProps }) {
+function Wall({ color, numTargets = [6, 6], ...restProps }) {
 	return (
 		<Plane scale={10} {...restProps}>
 			<meshPhongMaterial attach='material' color={color} />
 			{Array.from(Array(numTargets[0]).keys()).flatMap(i =>
 				Array.from(Array(numTargets[1]).keys()).map(j => {
-					const letter = String.fromCharCode(
-						Math.floor(Math.random() * 26) + 65
-					)
+					const letter = characterSet1[Math.floor(Math.random() * characterSet1.length)]
 					const x = (i / (numTargets[0] - 1)) * 0.8 - 0.4
 					const y = (j / (numTargets[1] - 1)) * 0.8 - 0.4
 					return (
 						<Text
 							key={`target-${i}-${j}`}
 							position={[x, y, 0.01]}
-							fontSize={0.05}
-							color='#000'
+							fontSize={0.15}
+							color='#000000'
 							anchorX='center'
 							anchorY='middle'
 						>
@@ -47,28 +40,44 @@ function App() {
 		<>
 			<Head title='Quantifying Immersion Experiment' />
 			<VRCanvas>
-				<Sky sunPosition={[0, 1, 0]} />
 				<ambientLight />
 				<pointLight position={[10, 10, 10]} />
 				<DefaultXRControllers />
 				<PositionLogger />
-				<Floor position={[0, -5, 0]} />
-				<Wall position={[0, 0, -5]} color='#00aa00' />
+        {/* Top wall */}
+				<Wall 
+          position={[0, 5, 0]} 
+          rotation={[Math.PI / 2, 0, 0]} 
+          color='#ececec' 
+        />
+        {/* Bottom wall */}
+        <Wall 
+          position={[0, -5, 0]} 
+          rotation={[-Math.PI / 2, 0, 0]} 
+          color='#ececec' 
+        />
+        {/* Back wall */}
+				<Wall 
+          position={[0, 0, -5]}
+          rotation={[0, 0, 0]}  
+          color='#ececec' />
+        {/* Right wall */}
 				<Wall
 					position={[5, 0, 0]}
 					rotation={[0, -Math.PI / 2, 0]}
-					color='#aaaa00'
+					color='#ececec'
 				/>
+        {/* Left wall */}
 				<Wall
 					position={[-5, 0, 0]}
 					rotation={[0, Math.PI / 2, 0]}
-					color='#aa0000'
+					color='#ececec'
 				/>
+        {/* Front wall */}
 				<Wall
 					position={[0, 0, 5]}
 					rotation={[0, Math.PI, 0]}
-					color='#0000aa'
-					numTargets={[0, 0]}
+					color='#ececec'
 				/>
 			</VRCanvas>
 		</>
