a6a96df7d53aab3271295761667d13201117057e
Henrique Dias <hacdias@gmail.com>
2021-10-18 10:36:50 +0200

    now its 4 by 4

--- src/pages/Experiment.jsx
+++ src/pages/Experiment.jsx
@@ -62,7 +62,7 @@ function Wall({
 	...restProps
 }) {
 	return (
-		<Plane scale={5} {...restProps}>
+		<Plane scale={4} {...restProps}>
 			<meshPhongMaterial attach='material' color={color} />
 			{Array.from(Array(numTargets[0]).keys()).flatMap(i =>
 				Array.from(Array(numTargets[1]).keys()).map(j => {
@@ -100,7 +100,7 @@ function Floor({
 	...restProps
 }) {
 	return (
-		<Plane scale={5} {...restProps}>
+		<Plane scale={4} {...restProps}>
 			<meshPhongMaterial attach='material' color={color} />
 			{Array.from(Array(numTargets[0]).keys()).map(i => {
 				const letter =
@@ -161,7 +161,7 @@ function Ceiling({
 	...restProps
 }) {
 	return (
-		<Plane scale={5} {...restProps}>
+		<Plane scale={4} {...restProps}>
 			<meshPhongMaterial attach='material' color={color} />
 			{Array.from(Array(numTargets[0]).keys()).map(i => {
 				const letter =
@@ -259,7 +259,7 @@ const VR = React.memo(function VR({ logFile }) {
 			{/* <Overlay /> */}
 			{/* Top wall */}
 			<Ceiling
-				position={[0, 5, 0]}
+				position={[0, 4, 0]}
 				rotation={[Math.PI / 2, 0, 0]}
 				color='#ececec'
 			/>
@@ -271,27 +271,27 @@ const VR = React.memo(function VR({ logFile }) {
 			/>
 			{/* Back wall */}
 			<Wall
-				position={[0, 2.5, -2.5]}
+				position={[0, 2, -2]}
 				rotation={[0, 0, 0]}
 				color='#ececec'
 				side='back'
 			/>
 			{/* Right wall */}
 			<Wall
-				position={[2.5, 2.5, 0]}
+				position={[2, 2, 0]}
 				rotation={[0, -Math.PI / 2, 0]}
 				color='#ececec'
 				side='right'
 			/>
 			{/* Left wall */}
 			<Wall
-				position={[-2.5, 2.5, 0]}
+				position={[-2, 2, 0]}
 				rotation={[0, Math.PI / 2, 0]}
 				color='#ececec'
 				side='left'
 			/>
 			{/* Front wall */}
-			<Wall position={[0, 2.5, 2.5]} rotation={[0, Math.PI, 0]} color='#ececec' />
+			<Wall position={[0, 2, 2]} rotation={[0, Math.PI, 0]} color='#ececec' />
 		</VRCanvas>
 	)
 })
